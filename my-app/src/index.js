deeaf6d2c619f2c28b15d48c74599162d087190a
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-09-06 22:03:36 +0200

    webpack configuration

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,1 +1,7 @@
-alert("Hello World");
+const newDiv = () => {
+    const div = document.createElement("div");
+    div.innerHTML = "<form><label>Email:<input type='text'></label><input type='submit'><form>" 
+    document.body.append(div); 
+}
+
+newDiv();
