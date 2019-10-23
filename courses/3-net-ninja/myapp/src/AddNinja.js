9c0953b519abaee90cf33c8a2ab25df9385cfb58
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-10-23 19:59:07 +0200

    net ninja project updates

---
+++ courses/3-net-ninja/myapp/src/AddNinja.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class AddNinja extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: null,
+            age: null,
+            belt: null
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name" >Name:</label>
+                    <input id="name" type="text" onChange={this.handleChange} />
+                    <label htmlFor="age">Age:</label>
+                    <input id="age" type="text" onChange={this.handleChange} />
+                    <label htmlFor="belt" >Belt:</label>
+                    <input id="belt" type="text" onChange={this.handleChange} />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
--- courses/3-net-ninja/myapp/src/App.js
+++ courses/3-net-ninja/myapp/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ninjas : [
+        { name: "Stefan", age: 25, belt: "orange", id:1 },
+        { name: "Iva", age: 20, belt: "black", id:2 },
+        { name: "Darko", age: 24, belt: "white", id:3 }
+      ]
+  }
+}
+  render() {
+    return (
+      <div className="App">
+        <h1>First react component</h1>
+        <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja/>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
