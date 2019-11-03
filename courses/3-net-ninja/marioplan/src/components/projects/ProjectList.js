37a0ac1aa4bc44ca29df7c67fa575c7ace11b90c
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-11-03 18:44:56 +0100

    marioplan components finished

---
+++ courses/3-net-ninja/marioplan/src/components/projects/ProjectList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
+
+const Projectlist = () => {
+    return (
+        <div className='project-list section'>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
+        </div>
+    )
+       
+}   
+
+export default Projectlist
