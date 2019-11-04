83422efa2967495ed23d870ea194831188bd75e4
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-11-04 15:39:46 +0100

    Update todoReducer.js

--- semos/src/redux/reducers/todoReducer.js
+++ semos/src/redux/reducers/todoReducer.js
@@ -9,8 +9,8 @@ export const todoReducer = (state = initState, action) => {
     switch (action.type) {
         case 'ADD_TODO': {
             console.log(action.todo)
-            const newTodos = state.todos.push(action.todo)
-            return { ...state, todos: newTodos }
+            // const newTodos = state.todos.push(action.todo)
+            // return { ...state, todos: newTodos }
         }
         default: {
             return state
